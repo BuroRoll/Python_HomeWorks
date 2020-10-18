cbccb0015e7fc949fca638f5ea5df17ad134cd7f
Danil Konkov <buroroll@gmail.com>
2020-10-18 20:00:17 +0500

    First homework

---
+++ homework1.py
@@ -0,0 +1,6 @@
+import numpy
+
+arr = numpy.random.randint(0, 1000, 1000)
+print(arr)
+max_element = numpy.amax(arr)
+print('Наибольший элемент массива случайных значений равен ' + str(max_element))
